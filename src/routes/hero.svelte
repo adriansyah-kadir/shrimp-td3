fdc3320a9ab64a22bd2289f2d17b3f3c9353b2b6
adriansyah-kadir <adriansyahkadir.work@gmail.com>
2025-11-01 08:56:16 +0800

    change title'

--- src/routes/hero.svelte
+++ src/routes/hero.svelte
@@ -1,9 +1,8 @@
 <div class="flex flex-col-reverse sm:flex-row items-start justify-between my-5">
   <div>
     <h2 class="text-2xl font-semibold sm:max-w-[50vw] mb-5">
-      PEMODELAN REINFORCEMENT LEARNING BERBASIS DATA OPTIMAL PARAMETER
-      INTEGRATION (DOPI) UNTUK PENGELOLAAN KUALITAS AIR DI LINGKUNGAN
-      <span class="text-green-400">TAMBAK UDANG</span>
+      PEMODELAN REINFORCEMENT LEARNING UNTUK OPTIMASI PARAMETER KUALITAS AIR
+      <span class="text-green-400">TAMBAK UDANG VANAME</span>
     </h2>
     <p class="text-muted-foreground max-w-[512px] text-wrap">
       Teknologi AI canggih menggunakan model Reinforcement Learning TD3 untuk
